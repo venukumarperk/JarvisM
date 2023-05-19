c71dcfac0b1e2ab58e1e65ca4c932344bab006cd
venukumarperk <125378929+venukumarperk@users.noreply.github.com>
2023-05-19 19:24:49 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://jarvisai-dn0x.onrender.com', {
+    const response = await fetch('https://jarvisai-dn0x.onrender.comhttps://friday-xom6.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
